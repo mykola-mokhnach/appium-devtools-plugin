362deb2597bd47597b3b001b4b478b98bd06630d
Mykola Mokhnach <mokhnach@gmail.com>
2023-09-16 08:58:56 +0200

    Better types

--- scripts/run-functional-tests.sh
+++ scripts/run-functional-tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+adb shell 'echo "chrome --disable-fre --no-default-browser-check --no-first-run" > /data/local/tmp/chrome-command-line'
+adb shell am set-debug-app --persistent com.android.chrome
+
 ARGS=(./test/functional/*.e2e.spec.js --exit --timeout 10m)
 npx mocha "${ARGS[@]}"
