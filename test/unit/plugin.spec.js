8d32735d087ab40ad5b656e387a0212b523040e2
Mykola Mokhnach <mokhnach@gmail.com>
2023-09-16 16:17:34 +0200

    fix more

--- test/unit/plugin.spec.js
+++ test/unit/plugin.spec.js
@@ -50,8 +50,8 @@ describe('DevtoolsPlugin', function () {
     _.isNil(plugin.driver).should.be.true;
     await plugin.handle(_.noop, driverWoAdb, 'createSession');
     _.isNil(plugin.adb).should.be.true;
-    _.isNil(plugin.serverInfo).should.be.false;
-    _.isNil(plugin.sessionId).should.be.false;
-    _.isNil(plugin.driver).should.be.false;
+    _.isNil(plugin.serverInfo).should.be.true;
+    _.isNil(plugin.sessionId).should.be.true;
+    _.isNil(plugin.driver).should.be.true;
   });
 });
