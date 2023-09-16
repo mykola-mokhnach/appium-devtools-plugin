c7f958522f4c12f9968589dd82a41377870dd99d
Mykola Mokhnach <mokhnach@gmail.com>
2023-09-16 20:33:19 +0200

    Fix path to tests

--- scripts/run-functional-tests.sh
+++ scripts/run-functional-tests.sh
@@ -3,5 +3,4 @@
 adb shell 'echo "chrome --disable-fre --no-default-browser-check --no-first-run" > /data/local/tmp/chrome-command-line'
 adb shell am set-debug-app --persistent com.android.chrome
 
-ARGS=(./test/functional/*.e2e.spec.js --exit --timeout 10m)
-npx mocha "${ARGS[@]}"
+npx mocha ./test/e2e/**/*.e2e.spec.js --exit --timeout 10m
