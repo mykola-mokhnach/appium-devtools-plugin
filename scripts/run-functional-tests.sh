31af8f1cf95cf6b7425369d4dc203d02b0478300
Mykola Mokhnach <mokhnach@gmail.com>
2026-01-16 08:59:42 +0100

    use ts

--- scripts/run-functional-tests.sh
+++ scripts/run-functional-tests.sh
@@ -3,4 +3,4 @@
 adb shell 'echo "chrome --disable-fre --no-default-browser-check --no-first-run" > /data/local/tmp/chrome-command-line'
 adb shell am set-debug-app --persistent com.android.chrome
 
-npx mocha ./test/e2e/**/*.e2e.spec.js --exit --timeout 10m
+npx mocha ./test/e2e/**/*.e2e.spec.ts --exit --timeout 10m
