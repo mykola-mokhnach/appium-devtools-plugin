bab212a619ef4608cd9dcacd88e3566d921ad989
Mykola Mokhnach <mokhnach@gmail.com>
2026-08-01 20:18:02 +0200

    chore: Drop chai

--- test/unit/utils.spec.ts
+++ test/unit/utils.spec.ts
@@ -1,6 +1,5 @@
+import assert from 'node:assert/strict';
 import {describe, it} from 'node:test';
-
-import {expect} from 'chai';
 
 import {replaceDeep} from '../../lib/utils.js';
 
@@ -20,7 +19,7 @@ describe('utils', function () {
         'ws=yolo:9222/session/devtools/page/DAB7FB6187B554E10B0BD18821265734',
       ],
     ];
-    expect(
+    assert.deepStrictEqual(
       replaceDeep(
         [
           {
@@ -45,26 +44,27 @@ describe('utils', function () {
         ],
         replaceMap,
       ),
-    ).to.eql([
-      {
-        description: '',
-        devtoolsFrontendUrl:
-          '/devtools/inspector.html?ws=yolo:9222/session/devtools/page/DAB7FB6187B554E10B0BD18821265734',
-        id: 'DAB7FB6187B554E10B0BD18821265734',
-        title: 'Yahoo',
-        type: 'page',
-        url: 'https://www.yahoo.com/',
-        webSocketDebuggerUrl: 'wss://yolo:9222/session/devtools/page/DAB7FB6187B554E10B0BD18821265734',
-      },
-      {
-        Browser: 'Chrome/72.0.3601.0',
-        'Protocol-Version': '1.3',
-        'User-Agent':
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3601.0 Safari/537.36',
-        'V8-Version': '7.2.233',
-        'WebKit-Version': '537.36 (@cfede9db1d154de0468cb0538479f34c0755a0f4)',
-        webSocketDebuggerUrl: 'wss://yolo:9222/session/devtools/browser/b0b8a4fb-bb17-4359-9533-a8d9f3908bd8',
-      },
-    ]);
+      [
+        {
+          description: '',
+          devtoolsFrontendUrl:
+            '/devtools/inspector.html?ws=yolo:9222/session/devtools/page/DAB7FB6187B554E10B0BD18821265734',
+          id: 'DAB7FB6187B554E10B0BD18821265734',
+          title: 'Yahoo',
+          type: 'page',
+          url: 'https://www.yahoo.com/',
+          webSocketDebuggerUrl: 'wss://yolo:9222/session/devtools/page/DAB7FB6187B554E10B0BD18821265734',
+        },
+        {
+          Browser: 'Chrome/72.0.3601.0',
+          'Protocol-Version': '1.3',
+          'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3601.0 Safari/537.36',
+          'V8-Version': '7.2.233',
+          'WebKit-Version': '537.36 (@cfede9db1d154de0468cb0538479f34c0755a0f4)',
+          webSocketDebuggerUrl: 'wss://yolo:9222/session/devtools/browser/b0b8a4fb-bb17-4359-9533-a8d9f3908bd8',
+        },
+      ],
+    );
   });
 });
